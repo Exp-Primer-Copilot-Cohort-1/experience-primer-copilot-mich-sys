459de590c2a5819489fcbc4bac0c3f8fe7168583
Ademeji Michael <85976163+mich-sys@users.noreply.github.com>
2023-09-22 18:43:32 +0100

    Delete member.js

--- member.js
+++
@@ -1,9 +0,0 @@
-function skillMember (member) {
-  return {
-    id: member.id,
-    name: member.name,
-    email: member.email,
-    phone: member.phone,
-    skills: member.skills.map(skill => skill.id)
-  }
-}
