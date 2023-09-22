539124af4d731dcbf065e6266f288a5b5c743759
Ademeji Michael <85976163+mich-sys@users.noreply.github.com>
2023-09-22 17:34:28 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,9 @@
-function skillMember () {
-    
+function skillMember (member) {
+  return {
+    id: member.id,
+    name: member.name,
+    email: member.email,
+    phone: member.phone,
+    skills: member.skills.map(skill => skill.id)
+  }
 }
